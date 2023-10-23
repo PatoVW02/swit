9bcbb88f5896e536e5daafb8e49c5ae11fdf02bf
Patricio <patovw@gmail.com>
2023-10-23 15:31:50 -0600

    firebase setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGgHoC8Pv5L4S-m9IEyeeGitpSPUWNzSs",
+  authDomain: "swit-31d1c.firebaseapp.com",
+  projectId: "swit-31d1c",
+  storageBucket: "swit-31d1c.appspot.com",
+  messagingSenderId: "411849823849",
+  appId: "1:411849823849:web:046d6a1d0e126857e0b4f8",
+  measurementId: "G-2H1Z73PYLE",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
