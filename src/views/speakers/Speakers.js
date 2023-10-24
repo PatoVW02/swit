5fc9eb585df5e549cf954a97c62bf0630ff602ac
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-10-23 18:05:48 -0600

    Alineado de speakers

--- src/views/speakers/Speakers.js
+++ src/views/speakers/Speakers.js
@@ -1,11 +1,11 @@
-import background from "../../assets/map_img_893643_1549392744.jpg";
+import BigImage from "../../assets/map_img_893643_1549392744.jpg";
 import "./Speakers.css"
 
 const Speakers = () => {
   return (
     <div className="Speakers">
-      <h1>Speakers</h1>
-      <img src={background} alt='background' className='background'></img>
+      <h1 className="Title">Speakers</h1>
+      <img src={BigImage} alt='background' className='background'></img>
     </div>
   );
 };
