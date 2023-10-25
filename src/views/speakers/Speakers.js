9f3789e37aa4e1fe3eacdfa3b97836ba0032163f
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-10-24 20:22:08 -0600

    xd

--- src/views/speakers/Speakers.js
+++ src/views/speakers/Speakers.js
@@ -1,4 +1,8 @@
 import BigImage from "../../assets/Hombros de gigantes.jpg";
+import DKnuth from "../../assets/Donald Knuth.jpg"
+import VCerf from "../../assets/sdvg.jpg"
+import Kleinrock from "../../assets/5c65b8535f583ad53b404ed7ea60c98e.png"
+
 import "./Speakers.css"
 
 const Speakers = () => {
@@ -6,6 +10,27 @@ const Speakers = () => {
     <div className="Speakers">
       <h1 className="Title">Speakers</h1>
       <img src={BigImage} alt='background' className='background'></img>
+      <p>
+
+      </p>
+
+      <h2 className="Descripcion">
+      Aquí encontramos individuos pioneros en las ciencias computacionales y líderes cuyas 
+      contribuciones colectivamente, han formado el mundo actual y la era digital en la que vivimos.
+      </h2>
+      <p>
+
+      </p>
+      
+
+      <img src={DKnuth} alt='Knuth' className='Speaker1'></img>
+
+      <img src={VCerf} alt='Knuth' className='Speaker2'></img>
+
+      <img src={Kleinrock} alt='Knuth' className='Speaker3'></img>
+
+
+
     </div>
   );
 };
